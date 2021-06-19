ea794cde24e17175d0763873693f86671495380b
Student <Student@192.168.2.41>
2021-06-18 20:16:13 -0400

    Test case

---
+++ Scripts/Virgin_assignment/Script1624061711017.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.virginmobile.ca/en/login/index.html?province=ON&geoResult=failed')
+
+WebUI.click(findTestObject('Object Repository/Page_Log in to My Account - Virgin Mobile Canada/a_TV'))
+
+WebUI.click(findTestObject('Object Repository/Page_Virgin TV - Virgin Mobile Canada/span_Mobile'))
+
+WebUI.click(findTestObject('Object Repository/Page_Virgin TV - Virgin Mobile Canada/a_Plans'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cell Phone Plans - Virgin Mobile Canada/span_Support'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cell Phone Plans - Virgin Mobile Canada/a_Billing  Usage'))
+
+WebUI.closeBrowser()
+
